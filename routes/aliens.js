831e62b234dade04b297482ae6b0268b968920a0
SaurabhSinghinnobit <saurabh.singh@innobitsystems.com>
2022-11-29 18:56:48 +0530

    Here change is update by Id

--- routes/aliens.js
+++ routes/aliens.js
@@ -75,6 +75,7 @@ router.patch('/:id',async(req,res) => {
 //         const alien = await Alien.findById(req.params.id)        
 //         if (typeof alien === 'undefined') {
 //             console.log("Variable is Undefined or data is not presents");
+//             throw Error("Hey Id can't found");
 //         }
 //         else {
 //         alien.sub = req.body.sub
@@ -88,49 +89,49 @@ router.patch('/:id',async(req,res) => {
 //     }
 // })
  
-router.put('/:id',async(req,res) => {
- 
-   try {
-       const alien = await Alien.findById(req.params.id)        
-       if (alien) {
-           alien.sub = req.body.sub
-           alien.name = req.body.name
-           alien.tech = req.body.tech
-           const a1 = await alien.save()
-           console.log(a1)
-           res.json(a1)
-       }
-       else{
-           console.log(`In alien object ${alien}data is not present`)
-       }
-   } catch (err) {
-       res.send("Error" + err)
-   }
-})
- 
-// Update the document by creating the object
- 
 // router.put('/:id',async(req,res) => {
  
-//     try {
-//         const alien = await Alien.findById(req.params.id)        
-//         if (alien) {
-//             let{
-//                 name,
-//                 tech,
-//                 sub
-//             } = req.query
-//             res.send(`name is ${name}, tech is ${tech} , sub is ${sub}`)
-//             db.alien.update({'name':'sagun'},{$set:{'name':'mahak'}})
-        
-//         }
-//         else{
-//             console.log(`In alien ${alien}data is not present`)
-//         }
-//     } catch (err) {
-//         res.send("Error" + err)
-//     }
+//    try {
+//        const alien = await Alien.findById(req.params.id)        
+//        if (alien) {
+//            alien.sub = req.body.sub
+//            alien.name = req.body.name
+//            alien.tech = req.body.tech
+//            const a1 = await alien.save()
+//            console.log(a1)
+//            res.json(a1)
+//        }
+//        else{
+//            console.log(`In alien object ${alien}data is not present`);
+//            throw Error("Hey Id can't found");
+//        }
+//    } catch (err) {
+//        res.send("Error" + err)
+//    }
 // })
+ 
+// Update the document by Id
+ 
+router.put('/:id',async(req,res) => {
+ 
+    try {
+        const alien = await Alien.findById(req.params.id)        
+        if (alien) {
+            
+            const result = await Alien.findByIdAndUpdate(req.params.id,{$set:{
+                'name': 'chawla',
+                'tech' : 'python'}
+            });
+            res.json(result);
+        }
+        else{
+            console.log(`In alien ${alien}data is not present`)
+            throw Error("Hey Id can't found");
+        }
+    } catch (err) {
+        res.send("Error" + err)
+    }
+})
  
 // DELETE Query
  
@@ -149,6 +150,7 @@ router.delete('/:id',async(req,res) => {
        }
        else{
            console.log(`In alien object ${alien}data is not present`)
+           throw Error("Hey Id can't found");
        }
    } catch (err) {
        res.send("Error" + err)
